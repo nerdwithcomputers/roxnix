62e3cf19d04bc5bbbdabc0fac70f172a365d05f5
m1raclemax <hpscigeek@gmail.com>
2026-07-05 16:49:11 -0400

    now we're getting somewhere

--- index.js
+++ index.js
@@ -2,32 +2,74 @@ var blessed = require('neo-neo-blessed');
 
 var screen = blessed.screen({
   smartCSR: true,
-  dock: true
+  dockBorders: true,
+  ignoreLocked: ['C-c', 'M-q']
 });
-screen.key(['escape','C-c'], (ch,key)=>{
+screen.key(['escape','M-c'], (ch,key)=>{
   return process.exit(0);
 });
 
-var input = blessed.form({
-  top: 'top',
-  left: 'center',
+var inputAssembly = blessed.form({
+  bottom: 3,
+  left: 1,
   width: '50%',
-  height: '50%',
+  height: 4,
   mouse: true,
+  keys: true,
+});
+
+var inputBox = blessed.Textbox({
+  parent: inputAssembly,
+  top: 0,
+  height: 3,
+  width: '100%-6',
+  inputOnFocus: true,
+  mouse: true,
+  keys: true,
+  content: ">",
+  border:{
+    type: 'line'
+  },
+  style: {
+    fg: 'green',
+    bg: 'black',
+    border:{
+      fg: 'yellow'
+    },
+  }
+});
+var inputGo = blessed.button({
+  parent: inputAssembly,
+  content: 'GO',
+  top: 0,
+  right: 0,
+  width: 6,
+  height: 3,
   border: {
     type: 'line'
+  },
+  padding:{
+    left: 1
+  },
+  style:{
+    fg: 'green',
+    bg: 'black',
+    border: {
+      fg: 'yellow'
+    },
+    hover: {
+      bg: 'red'
+    }
   }
 });
 
-var 
-
-screen.append(input);
+screen.append(inputAssembly);
 
 var out = blessed.list({
-  top: 'center',
-  left: 'center',
+  top: 3,
+  left: 1,
   width: '50%',
-  height: '50%',
+  height: '70%',
   border:{
     type: 'line'
   },
@@ -35,7 +77,7 @@ var out = blessed.list({
     "hello there"
   ]
 });
-// screen.append(out);
+screen.append(out);
 
-input.focus();
+inputBox.focus();
 screen.render();
